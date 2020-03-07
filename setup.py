9e02cef225f19539d0f4f69706c4715ee9b802f2
Eric Dang <edang830@gmail.com>
2020-03-07 12:03:26 -0800

    Update version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name="confbusterplusplus",
-    version="1.0.0",
+    version="1.1.0",
     author="Eric Dang",
     description=("A macrocycle conformational sampling tool."),
     license="MIT",
