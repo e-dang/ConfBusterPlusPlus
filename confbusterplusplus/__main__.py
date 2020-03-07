8d15e7d43e4e82926332d51dd68525e7362f0942
Eric Dang <edang830@gmail.com>
2020-03-06 20:44:45 -0800

    Move Runner class to __main__

--- confbusterplusplus/__main__.py
+++ confbusterplusplus/__main__.py
@@ -25,8 +25,210 @@ github - https://github.com/e-dang
 """
 
 import argparse
+import json
+import os
+import pprint
+from time import time
 
-from confbusterplusplus.runner import Runner
+import numpy as np
+from rdkit import Chem
+from rdkit.Chem import AllChem
+
+import confbusterplusplus.exceptions as exceptions
+import confbusterplusplus.utils as utils
+from confbusterplusplus.factory import ConfBusterFactory
+from confbusterplusplus.utils import terminate
+
+pprint.sorted = lambda x, key=None: x  # disables sorting of dict keys in pprint.pprint()
+
+
+class Runner:
+    """
+    Class for taking parsed commandline arguments from the argparse module, validating them, and subsequently creating
+    the ConformerGenerator class with those arguments and outputting the resulting conformers and statistics.
+    """
+
+    def __init__(self, args):
+        """
+        Intializer.
+
+        Args:
+            args (Namespace): The namespace object returned by the command line argument parser.
+        """
+
+        self.args = args
+
+        self.factory = ConfBusterFactory(**vars(self.args))
+        self.mols = []
+        self.output_pdb = ''
+        self.output_json = ''
+        self.params = {}  # parameters to initialize ConformerGenerator with.
+        self.run()
+
+    def run(self):
+        """
+        Top level function that validates the command line arguments, creates the ConformerGenerator, runs the
+        conformational sampling process, and saves the output.
+        """
+
+        try:
+            self.parse_inputs()
+            self.parse_outputs()
+        except RuntimeWarning as warning:
+            print(warning)
+
+        generator = self.factory.create_conformer_generator()
+        for mol in self.mols:
+            print(f'Generating conformers for molecule: {Chem.MolToSmiles(mol)}')
+
+            try:
+                start = time()
+                confs, energies, rmsd, ring_rmsd, num_cleavable_bonds, num_ring_atoms = generator.generate(mol)
+                finish = time() - start
+            except exceptions.InvalidMolecule:
+                print('Failed to find ring with at least {generator.MIN_MACRO_RING_SIZE} atoms in molecule '
+                      f'{Chem.MolToSmiles(mol)}.')
+                continue
+
+            params = self.factory.get_parameters()
+            Chem.MolToPDBFile(confs, utils.file_rotator(self.output_pdb))
+            self.write_stats(confs, energies, rmsd, ring_rmsd, num_cleavable_bonds, num_ring_atoms, finish, params)
+            self.print_stats(confs, energies, rmsd, ring_rmsd, num_cleavable_bonds, num_ring_atoms, finish, params)
+
+    def parse_inputs(self):
+        """
+        Helper function that validates the command line arguments regarding the input macrocycles to the
+        ConformerGenerator.
+        """
+
+        self.validate_inputs()
+
+        # create mol(s) from SMILES string
+        if self.args.smiles:
+            self.mols.extend([Chem.MolFromSmiles(smiles) for smiles in self.args.smiles])
+
+        # load mol(s) from file
+        if self.args.sdf:
+            self.mols.extend([Chem.MolFromSmiles(Chem.MolToSmiles(mol)) for mol in Chem.SDMolSupplier(self.args.sdf)])
+
+    def validate_inputs(self):
+        """
+        Ensures that there is at least one command line argument given that specifies the source of the input
+        macrocycles.
+        """
+
+        if not (self.args.smiles or self.args.sdf):  # check if no inputs are given
+            terminate('Error. No input provided, please provide either a SMILES string with option --smiles or a '
+                      'filepath to an sdf containing the macrocycles with option --sdf.', 1)
+
+    def parse_outputs(self):
+        """
+        Helper function that validates the commmand line arguments regarding the output file for writing the conformers
+        to. Also generates a .txt file name for writing run statistic based on the supplied .pdb file name.
+        """
+
+        self.validate_outputs()
+
+        self.output_pdb = self.args.out
+        self.output_json = os.path.join(os.path.split(self.args.out)[0], os.path.splitext(
+            os.path.basename(self.args.out))[0] + '.json')
+
+    def validate_outputs(self):
+        """
+        Ensures that exactly one output filepath, with a .pdb extension, is given and that the filepath pointing to the
+        file location exists.
+        """
+
+        try:
+            _, ext = os.path.splitext(self.args.out)
+        except AttributeError:
+            terminate('Error. Must supply an output pdb output file.', 1)
+
+        path, _ = os.path.split(self.args.out)
+        if path == '':
+            path = './'
+        if not os.path.isdir(path):
+            terminate('Error. Cannot write to the given output file because a directory in the given path does '
+                      'not exist.', 1)
+
+        if ext != '.pdb':
+            terminate('Error. The output file must have a .pdb file extension.', 1)
+
+    def write_stats(self, mol, energies, rmsd, ring_rmsd, num_cleavable_bonds, num_ring_atoms, finish, params):
+        """
+        Function that writes the run statistics to the provided .json file.
+
+        Args:
+            mol (RDKit Mol): The molecule used in the conformational sampling.
+            energies (list): A list of the conformer energies (kcal/mol).
+            rmsd (list): A list of RMSD values between each conformer and the lowest energy conformer (Å).
+            ring_rmsd (list): A list of ring RMSD values between each conformer and the lowest energy conformer (Å).
+            num_cleavable_bonds (int): The number of cleavable bonds found in the macrocycle.
+            num_ring_atoms (int): The size of the macrocyclic ring(s) in atoms.
+            finish (float): The total time it took to complete the conformational sampling process (s).
+            params (dict): The parameters that the ConformerGenerator used to generate the set of conformers.
+        """
+
+        with open(utils.file_rotator(self.output_json), 'w') as file:
+            data = {'SMILES': Chem.MolToSmiles(Chem.RemoveHs(mol)),
+                    'num_confs': mol.GetNumConformers(),
+                    'num_rotatable_bonds': AllChem.CalcNumRotatableBonds(mol, True),
+                    'num_cleavable_bonds': num_cleavable_bonds,
+                    'num_ring_atoms': num_ring_atoms,
+                    'time': finish,
+                    'energies': energies,
+                    'rmsd': rmsd,
+                    'ring_rmsd': ring_rmsd,
+                    'parameters': params}
+            json.dump(data, file)
+
+    def print_stats(self, mol, energies, rmsd, ring_rmsd, num_cleavable_bonds, num_ring_atoms, finish, params):
+        """
+        Helper function that formats and prints the run statistics to the console.
+
+        Args:
+            mol (RDKit Mol): The molecule used in the conformational sampling.
+            energies (list): A list of the conformer energies (kcal/mol).
+            rmsd (list): A list of RMSD values between each conformer and the lowest energy conformer (Å).
+            ring_rmsd (list): A list of ring RMSD values between each conformer and the lowest energy conformer (Å).
+            num_cleavable_bonds (int): The number of cleavable bonds found in the macrocycle.
+            num_ring_atoms (int): The size of the macrocyclic ring(s) in atoms.
+            finish (float): The total time it took to complete the conformational sampling process (s).
+            params (dict): The parameters that the ConformerGenerator used to generate the set of conformers.
+        """
+
+        print(f'SMILES: {Chem.MolToSmiles(Chem.RemoveHs(mol))}')
+        print(f'Number of Conformers: {mol.GetNumConformers()}')
+        print(f'Number of Rotatable Bonds: {AllChem.CalcNumRotatableBonds(mol, True)}')
+        print(f'Number of Cleavable Bonds: {num_cleavable_bonds}')
+        print(f'Number of Macrocyclic Ring Atoms: {num_ring_atoms}')
+        print(f'Time: {finish} seconds')
+        self._print_stat(energies, 'Energy', 'kcal/mol')
+        self._print_stat(rmsd, 'RMSD', 'Å')
+        self._print_stat(ring_rmsd, 'Ring_RMSD', 'Å')
+        print(f' Parameter List '.center(80, '-') + '\n')
+        pprint.pprint(params)
+
+    def _print_stat(self, stats, stat_name, units):
+        """
+        Helper function that prints the given statistic in a certain format.
+
+        Args:
+            stats (list): The list of numbers that compose this statistic.
+            stat_name (str): The name of the statistic.
+            units (str): The units that the statistic is measured in.
+        """
+
+        print(f' {stat_name} ({units}) '.center(80, '-'))
+        for stat in stats:
+            print(stat)
+
+        try:
+            print(f'Average: {np.average(stats)}')
+            print(f'Standard Deviation: {np.std(stats)}')
+        except RuntimeWarning:
+            print('Average: Nan')
+            print('Standard Deviation: Nan')
 
 
 def main():
